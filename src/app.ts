fdb14fb690118d1e30effcf0490a78d064b26ef9
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-05-13 16:37:01 -0300

    feat: Sobe endpoint GET /products

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+import route from './Routes/Products.route';
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/products', route);
 
 export default app;
