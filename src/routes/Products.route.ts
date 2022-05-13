cd1216357c85aeec9f67803cff992d680d1f22fa
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-05-13 19:19:17 -0300

    feat: Implementa endpoint POST /products

--- src/routes/Products.route.ts
+++ src/routes/Products.route.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 import ProductsController from '../controllers/Products.controller';
+import joiValidate from '../middlewares/joiValidate';
+import { products } from '../utils/joiSchemas';
 
 const route = Router();
 const controller = new ProductsController();
 
 route.get('/', controller.getAll);
+
+route.post('/', joiValidate(products), controller.create);
 
 export default route;
