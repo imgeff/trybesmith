95f5ad8c4d89aee5ad009201fd9c3efb072c3159
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-05-13 17:15:20 -0300

    feat: Cria método create na class ProductModel

--- src/models/Products.model.ts
+++ src/models/Products.model.ts
@@ -1,13 +1,21 @@
+import { ResultSetHeader } from 'mysql2/promise';
 import connection from './connection';
-import { AllProducts } from '../interfaces/Products.interface';
+import { AllProducts, NewProduct } from '../interfaces/Products.interface';
 
 const productsQuery: any = {
   getAll: 'SELECT * FROM Trybesmith.Products',
+  create: 'INSERT INTO Trybesmith.Products (name, amount) VALUES(?, ?)',
 };
 
 export default class ProductsModel {
   public getAll = async (): Promise<AllProducts[]> => {
     const [products] = await connection.execute(productsQuery.getAll);
     return products as AllProducts[];
+  };
+
+  public create = async (name: string, amount: string): Promise<NewProduct> => {
+    const [rows] = await connection.execute<ResultSetHeader>(productsQuery.create, [name, amount]);
+    const { insertId } = rows;
+    return { id: insertId, name, amount };
   };
 }
