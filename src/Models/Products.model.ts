a68eaadfad67d7f3af78aade63a182265f9cf9f0
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-05-13 16:07:59 -0300

    feat: Cria model de Products e metodo getAll

---
+++ src/Models/Products.model.ts
@@ -0,0 +1,13 @@
+import connection from './connection';
+import { AllProducts } from '../Interfaces/Products.interface';
+
+const productsQuery: any = {
+  getAll: 'SELECT * FROM Trybesmith.Products',
+};
+
+export default class ProductsModel {
+  public getAll = async (): Promise<AllProducts[]> => {
+    const [products] = await connection.execute(productsQuery.getAll);
+    return products as AllProducts[];
+  };
+}
