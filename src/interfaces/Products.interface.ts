f47a680b8fa227ce7e6788b07ab8a9c369f075ef
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-05-13 17:14:23 -0300

    refactor&feat: Cria interface NewProduct e refatora AllProducts

--- src/interfaces/Products.interface.ts
+++ src/interfaces/Products.interface.ts
@@ -1,6 +1,9 @@
-export interface AllProducts {
+export interface NewProduct {
   id: number;
   name: string;
   amount: string;
+}
+
+export interface AllProducts extends NewProduct {
   orderId: number | null;
 }
