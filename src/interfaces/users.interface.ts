bfaf3d88c50de6ebb8c5c09867f947a47533d1ad
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-05-13 23:51:20 -0300

    feat: Cria interface extendida de Request
    Nova interface utiliza extends para adicionar objeto user com chaves code e userId ao type Request do express

--- src/interfaces/users.interface.ts
+++ src/interfaces/users.interface.ts
@@ -1,6 +1,12 @@
+import { Request } from 'express';
+
 export interface NewUser {
   username: string;
   classe: string;
   level: number;
   password: string;
+}
+
+export interface NewUserRequest extends Request {
+  user: { code: number, userId: number };
 }
