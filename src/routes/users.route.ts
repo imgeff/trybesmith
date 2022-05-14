d794a1d1a38888dead3799cd9eba0e44241005ea
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-05-14 12:06:26 -0300

    feat: Adiciona middleware de validação na rota POST /users
    utiliza middleware Joi e schema users para validar

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express';
 import UsersController from '../controllers/users.controller';
 import Token from '../middlewares/token';
+import joiValidate from '../middlewares/validations/joiValidate';
+import { users } from '../utils/joiSchemas';
 
 const controller = new UsersController();
 const token = new Token();
 
 const route = Router();
 
-route.post('/', controller.create, token.generate);
+route.post('/', joiValidate(users), controller.create, token.generate);
 
 export default route;
