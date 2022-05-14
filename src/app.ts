144d201cb600a5b1c8371d0014158b22ca5415f5
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-05-14 11:54:41 -0300

    feat: Implementa Rotas no endpoint /users

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,15 @@
 import express from 'express';
-import route from './routes/products.route';
+import routeProducts from './routes/products.route';
+import routeUsers from './routes/users.route';
 import manageErrors from './middlewares/errors/manageErrors';
 
 const app = express();
 
 app.use(express.json());
 
-app.use('/products', route);
+app.use('/products', routeProducts);
+
+app.use('/users', routeUsers);
 
 app.use(manageErrors);
 export default app;
