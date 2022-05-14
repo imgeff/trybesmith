12414cad8a08d02b88e663be987c888e405e1319
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-05-14 20:13:42 -0300

    refactor: metódo getAll em OrdersModel
    Cria propriedade prouctsIds em cada pedido que guarda todos os produtos de cada pedido

--- src/models/orders.model.ts
+++ src/models/orders.model.ts
@@ -1,16 +1,28 @@
+import { RowDataPacket } from 'mysql2/promise';
 import connection from './connection';
+import ProductsModel from './products.model';
 import { AllOrders } from '../interfaces/orders.interface';
+import { AllProducts } from '../interfaces/products.interface';
 
 const ordersQuery = {
-  getAll: `SELECT ORD.*, PROD.id 
-  FROM Trybesmith.Orders AS ORD 
-  INNER JOIN Trybesmith.Products AS PROD
-  ON ORD.id = PROD.orderId`,
+  getAll: 'SELECT * FROM Trybesmith.Orders',
 };
 
 export default class OrdersModel {
+  private productsModel = new ProductsModel();
+
   public getAll = async (): Promise<AllOrders[]> => {
-    const [orders] = await connection.execute(ordersQuery.getAll);
+    const allProducts = await this.productsModel.getAll();
+    const [orders] = await connection.execute<RowDataPacket[]>(ordersQuery.getAll, []);
+    
+    const ordersIds = orders.map((order) => order.id);
+    ordersIds.forEach((id, index) => {
+      const products: AllProducts[] = allProducts.filter((product) => product.orderId === id);
+      const productsIds: number[] = [];
+      products.forEach((product) => productsIds.push(product.id));
+      orders[index].productsIds = productsIds;
+    });
+
     return orders as AllOrders[];
   };
 }
