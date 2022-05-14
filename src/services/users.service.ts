c211ca6ae8a7745e14c5bb2bf13a3ffab6e5e5ef
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-05-13 23:37:12 -0300

    feat: Cria class UsersService com metódo create
    metódo create irá passar as informações do novo usuário para que a camada de model possa inserir no banco

---
+++ src/services/users.service.ts
@@ -0,0 +1,11 @@
+import UsersModel from '../models/users.model';
+import { NewUser } from '../interfaces/users.interface';
+
+export default class UsersService {
+  private model = new UsersModel();
+
+  public create = async (newUser: NewUser): Promise<number> => {
+    const newUserId = await this.model.create(newUser);
+    return newUserId;
+  };
+}
