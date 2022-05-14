786af395a209cde6d62e3362c205ca62eadeef85
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-05-14 20:15:19 -0300

    feat: Sobe rotas /orders na aplicação

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import routeProducts from './routes/products.route';
 import routeUsers from './routes/users.route';
+import routeOrders from './routes/orders.route';
 import manageErrors from './middlewares/errors/manageErrors';
 
 const app = express();
@@ -10,6 +11,8 @@ app.use(express.json());
 app.use('/products', routeProducts);
 
 app.use('/users', routeUsers);
+
+app.use('/orders', routeOrders);
 
 app.use(manageErrors);
 export default app;
