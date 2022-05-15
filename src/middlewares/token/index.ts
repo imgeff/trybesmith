a09955288fbd62d7f6f25e9f233991ed501de954
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-05-15 16:28:27 -0300

    feat: Cria metódo verify na class Token

--- src/middlewares/token/index.ts
+++ src/middlewares/token/index.ts
@@ -1,22 +1,27 @@
-import jwt, { Algorithm } from 'jsonwebtoken';
-import { Request, Response } from 'express';
+import jwt, { VerifyOptions, SignOptions } from 'jsonwebtoken';
+import { NextFunction, Request, Response } from 'express';
 
-interface IJwtConfig {
-  algorithm: Algorithm;
-  expiresIn: string;
-}
-
-const jwtConfig: IJwtConfig = {
+const jwtConfig = {
   algorithm: 'HS256',
   expiresIn: '30m',
 };
 
 export default class Token {
+  private secret = process.env.SECRET || 'Trybesmith*';
+
   public generate = (req: Request, res: Response): Response => {
     const { code, payload } = req.body;
-    const secret = process.env.SECRET || 'Trybesmith*';
-    const token = jwt.sign(payload, secret, jwtConfig);
+    const token = jwt.sign(payload, this.secret, jwtConfig as SignOptions);
 
     return res.status(code).json({ token });
+  };
+
+  public verify = (req: Request, res: Response, next: NextFunction): Response | void => {
+    const { authorization } = req.headers;
+    const { productsIds } = req.body;
+    if (!authorization) return res.status(401).json({ message: 'Token not found' });
+    const decoded = jwt.verify(authorization, this.secret, jwtConfig as VerifyOptions) as any;
+    req.body = { id: decoded.id, productsIds };
+    return next();
   };
 }
