a3c7ba29e05cde39889bec78e08dd868550398bc
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-05-15 20:29:47 -0300

    feat: Cria metódo create na class OrdersService

--- src/services/orders.service.ts
+++ src/services/orders.service.ts
@@ -1,5 +1,5 @@
 import OrdersModel from '../models/orders.model';
-import { AllOrders } from '../interfaces/orders.interface';
+import { AllOrders, NewOrder } from '../interfaces/orders.interface';
 
 export default class OrdersService {
   private model = new OrdersModel();
@@ -7,5 +7,10 @@ export default class OrdersService {
   public getAll = async (): Promise<AllOrders[]> => {
     const orders = await this.model.getAll();
     return orders;
+  };
+
+  public create = async (newOrder: NewOrder): Promise<NewOrder> => {
+    const orderCreated = await this.model.create(newOrder);
+    return orderCreated;
   };
 }
