cbffaca8363ca83c834664f1a570171d3d27ab25
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-05-15 22:15:42 -0300

    feat: Implementa rota Post /orders

--- src/routes/orders.route.ts
+++ src/routes/orders.route.ts
@@ -1,9 +1,15 @@
 import { Router } from 'express';
 import OrdersController from '../controllers/orders.controller';
+import Token from '../middlewares/token';
+import joiValidate from '../middlewares/validations/joiValidate';
+import { orders } from '../utils/joiSchemas';
 
 const route = Router();
 const controller = new OrdersController();
+const token = new Token();
 
 route.get('/', controller.getAll);
+
+route.post('/', token.verify, joiValidate(orders), controller.create);
 
 export default route;
